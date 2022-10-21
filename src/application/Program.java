74ff4e587a7bc2fdc7cafe82585c3244eb9d8af9
Juliana-Ferreira <juliana_cristina.ferreira@hotmail.com>
2022-10-21 11:53:01 -0300

    Classe da entidade Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department department = new Department(1, "RH");
+		Seller seller = new Seller(18, "Julia Ramos", "julia@gmail.com", new Date(), 1200.00, department);
+		
 		System.out.println(department);
+		System.out.println(seller);
 		
 	}
 }
