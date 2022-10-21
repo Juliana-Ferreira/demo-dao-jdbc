20f24f47e49ce10e9a56580a9326830f9efafb9e
Juliana-Ferreira <juliana_cristina.ferreira@hotmail.com>
2022-10-21 11:41:13 -0300

    Classe da entidade Department

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department department = new Department(1, "RH");
+		System.out.println(department);
+		
+	}
+}
