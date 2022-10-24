0b12a13810fdb8f71131641e64773d0afb9af7bf
Juliana-Ferreira <juliana_cristina.ferreira@hotmail.com>
2022-10-24 14:05:26 -0300

    Classes SellerDaoJDBC e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
 
 		Department department = new Department(1, "RH");
 		Seller seller = new Seller(18, "Julia Ramos", "julia@gmail.com", new Date(), 1200.00, department);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(department);
 		System.out.println(seller);
