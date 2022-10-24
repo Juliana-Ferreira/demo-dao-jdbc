882c6cc818b2fc945194e5747fb398da605f2997
Juliana-Ferreira <juliana_cristina.ferreira@hotmail.com>
2022-10-24 14:40:56 -0300

    Implementando findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,16 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-
-		Department department = new Department(1, "RH");
-		Seller seller = new Seller(18, "Julia Ramos", "julia@gmail.com", new Date(), 1200.00, department);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
 		
-		System.out.println(department);
 		System.out.println(seller);
-		
 	}
 }
