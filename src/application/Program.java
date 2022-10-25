6feb2212df1abbb644dd2ca922528ba7a5c3fbfc
Juliana-Ferreira <juliana_cristina.ferreira@hotmail.com>
2022-10-25 10:54:26 -0300

    Implementando findByDepartment e melhoria na reutilização do código

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -9,8 +12,16 @@ public class Program {
 	public static void main(String[] args) {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		Seller seller = sellerDao.findById(3);
 		
+		System.out.println("-----------> Teste 1 - findById: <-----------");
+		Seller seller = sellerDao.findById(5);
 		System.out.println(seller);
+		
+		System.out.println("\n-----------> Teste 2 - findByDepartment: <-----------");
+		Department department = new Department(1, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for(Seller x : list) {
+			System.out.println(x);
+		}
 	}
 }
